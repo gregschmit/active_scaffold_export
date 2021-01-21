43ea81ab8fea46529972633318a356f096ea4255
Gregory N. Schmit <schmitgreg@gmail.com>
2021-01-21 17:07:02 -0600

    Attempt to fix routing error.

--- lib/active_scaffold_export/config/core.rb
+++ lib/active_scaffold_export/config/core.rb
@@ -1,3 +1,5 @@
+require 'active_scaffold/extensions/routing_mapper'
+
 # Need to open the AS module carefully due to Rails 2.3 lazy loading
 ActiveScaffold::Config::Core.class_eval do
   # For some note obvious reasons, the class variables need to be defined
@@ -9,10 +11,10 @@ ActiveScaffold::Config::Core.class_eval do
   self.send :class_variable_set, :@@export_default_skip_header, false
   self.send :class_variable_set, :@@export_default_delimiter, ','
   self.send :class_variable_set, :@@export_default_file_format, Gem::Specification::find_all_by_name('axlsx_rails').any? ? 'xlsx' : 'csv'
-  
+
   cattr_accessor :export_show_form, :export_allow_full_download,
       :export_force_quotes, :export_default_full_download,
-      :export_default_delimiter, :export_default_skip_header, 
+      :export_default_delimiter, :export_default_skip_header,
       :export_default_file_format, :export_xlsx_avaliable
 
   ActionDispatch::Routing::ACTIVE_SCAFFOLD_CORE_ROUTING[:collection][:show_export] = :get
